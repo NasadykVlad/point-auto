af3ba3bc07d3320289ba2291462445bfdebeadd4
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2022-02-12 18:15:16 +0200

    add-404

--- frontend/src/components/404/404.js
+++ frontend/src/components/404/404.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './404.scss'
 
 const NotFound = () => {
+
     return (
-        <div>
-            <h1>404 - Not Found Page</h1>
+        <div className="NotFound">
+            <figure>
+                <div className="sad-mac"></div>
+                <figcaption>
+                    <span className="sr-text">Error 404: Not Found</span>
+                    <span className="e"></span>
+                    <span className="r"></span>
+                    <span className="r"></span>
+                    <span className="o"></span>
+                    <span className="r"></span>
+                    <span className="_4"></span>
+                    <span className="_0"></span>
+                    <span className="_4"></span>
+                    <span className="n"></span>
+                    <span className="o"></span>
+                    <span className="t"></span>
+                    <span className="f"></span>
+                    <span className="o"></span>
+                    <span className="u"></span>
+                    <span className="n"></span>
+                    <span className="d"></span>
+                </figcaption>
+            </figure>
         </div>
     );
 };
